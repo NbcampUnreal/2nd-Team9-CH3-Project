643012767095fe3f1e97164a1e29d6888cb7492c
seungHyeonL <hkr0416@gmail.com>
2025-02-25 19:08:49 +0900

    [승현][추가] Wraith 애님인스턴스  C++ 클래스 생성, 그거 기반 ABP 생성

---
+++ Source/HelloWorld/Public/Types.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Types.generated.h"
+
+UENUM(BlueprintType)
+enum class EFireState : uint8
+{
+	Waiting, Aiming, Firing
+};
+
+UENUM(BlueprintType)
+enum class EChargeState : uint8
+{
+	Normal, Medium, Full
+};
