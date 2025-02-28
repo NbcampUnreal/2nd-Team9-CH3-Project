5c4a12777e0c5017bf08e6e20fbec241235670d5
choidahey <daheychoe2000@gmail.com>
2025-03-01 01:22:23 +0900

    [다혜][추가] 보스 기본 베이스 추가

---
+++ Source/HelloWorld/Public/BossCharacter.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BossCharacter.generated.h"
+
+UCLASS()
+class HELLOWORLD_API ABossCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABossCharacter();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Boss|Status")
+	bool bIsDead;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Boss|Combat")
+	int32 MaxHp;
+
+	UPROPERTY(EditAnywhere, Category = "Boss|Combat")
+	int32 CurrentHp;
+
+	UPROPERTY(EditAnywhere, Category = "Boss|Combat")
+	int32 AttackPower;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Boss|Animation")
+	UAnimMontage* HitMontage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Boss|Animation")
+	UAnimMontage* AttackMontage;
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable, Category = "Boss|Status")
+	void Attack();
+
+	UFUNCTION(BlueprintCallable, Category = "Boss|Status")
+	virtual float TakeDamage(
+		float DamageAmount,
+		FDamageEvent const& DamageEvent,
+		AController* EventInstigator,
+		AActor* DamageCauser
+	) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Boss|Status")
+	void Die();
+
+	void DestroyEnemy();
+
+};
