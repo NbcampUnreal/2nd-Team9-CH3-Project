72f1585b0785f4c137d51656dfb5e3f60108a7cc
eomcri <jihun.eomcri@gmail.com>
2025-03-06 11:59:57 +0900

    [지훈][추가] 체력 회복 구 구현

--- Source/HelloWorld/Private/3_Inventory/HealthPack.cpp
+++ Source/HelloWorld/Private/3_Inventory/HealthPack.cpp
@@ -3,10 +3,45 @@
 
 #include "3_Inventory/HealthPack.h"
 
+#include "4_Character/ParagonAssetCharacter.h"
+
 AHealthPack::AHealthPack()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	if (!StaticMesh)
+	{
+		StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("StaticMesh");
+		ConstructorHelpers::FObjectFinder<UStaticMesh> SphereMesh(TEXT("/Engine/BasicShapes/Sphere.Sphere"));
+		if (SphereMesh.Succeeded())
+		{
+			StaticMesh->SetStaticMesh(SphereMesh.Object);
+		}
+		static ConstructorHelpers::FObjectFinder<UMaterial> HelthPackMaterial(TEXT("/Game/_Dev/juhun/M_HealthPack.M_HealthPack"));
+		if (HelthPackMaterial.Succeeded())
+		{
+			StaticMesh->SetMaterial(0, HelthPackMaterial.Object);
+		}
+		StaticMesh->SetupAttachment(RootComponent);
+		StaticMesh->OnComponentBeginOverlap.AddDynamic(this, &AHealthPack::OnInteract);
+		StaticMesh->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+	}
 	
+
+	HealingAmount = 20;
+	DropChance = 10;
+	DropPointOffset_Z = 10.0f;
+
+	RootComponent->SetRelativeScale3D(FVector(0.6f, 0.6f, 0.6f));
+}
+
+void AHealthPack::BeginPlay()
+{
+	Super::BeginPlay();
+	if (FMath::FRand() > DropChance)
+	{
+		Destroy();
+	}
 }
 
 void AHealthPack:: OnInteract(UPrimitiveComponent* OverlappedComponent,
@@ -16,5 +51,25 @@ void AHealthPack:: OnInteract(UPrimitiveComponent* OverlappedComponent,
 	bool bFromSweep,
 	const FHitResult& SweepResult)
 {
-	
+	if (OtherActor && OtherActor->ActorHasTag("Player"))
+	{
+		AParagonAssetCharacter* Player = Cast<AParagonAssetCharacter>(OtherActor);
+		// 승현님 SetCurrentHealth 함수 추가해주세요
+		// Player->SetCurrentHealth(Player->GetCurrentHealth() + HealingAmount);
+		UE_LOG(LogTemp, Display, TEXT("Healing Player %d"), HealingAmount);
+		Destroy();
+	}
 }
+
+// 다혜님 Enemy 죽는 곳에 아래 코드 추가해주세요
+// Enemy가 죽을 때 해당 위치에 HealthPack 생성
+// AHealthPack* HealthPack = GetWorld()->SpawnActor<AHealthPack>();
+// if(HealthPack)
+// {
+// 	HealthPack->SetDropLocation(this->GetActorLocation);
+// }
+void AHealthPack::SetDropLocation(const FVector& EnemyLocation)
+{
+	this->SetActorLocation(EnemyLocation + FVector3d::UnitZ() * DropPointOffset_Z);
+}
+
--- Source/HelloWorld/Public/3_Inventory/HealthPack.h
+++ Source/HelloWorld/Public/3_Inventory/HealthPack.h
@@ -11,6 +11,21 @@ UCLASS()
 class HELLOWORLD_API AHealthPack : public AActor, public IInteractInterface
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Static Mesh")
+	UStaticMeshComponent* StaticMesh;
+	// 체력 회복량
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="HealthPack")
+	int32 HealingAmount;
+	// Drop 확률
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="HealthPack")
+	float DropChance;
+	// Drop할 z축 위치 offset
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="HealthPack")
+	int32 DropPointOffset_Z;
+
+	virtual void BeginPlay() override;
 	
 public:	
 	// Sets default values for this actor's properties
@@ -22,4 +37,6 @@ public:
 		int32 OtherBodyIndex,
 		bool bFromSweep,
 		const FHitResult& SweepResult) override;
+	
+	void SetDropLocation(const FVector& EnemyLocation);
 };
