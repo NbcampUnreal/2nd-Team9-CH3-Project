34f5d4c1fd0a763331a6c364cb26112f0bdcd1ef
eomcri <jihun.eomcri@gmail.com>
2025-03-07 02:37:37 +0900

    [지훈][수정] 에임할 때 HP Bar에 영향 받는 버그 해결

--- Source/HelloWorld/Private/3_Inventory/Bullet.cpp
+++ Source/HelloWorld/Private/3_Inventory/Bullet.cpp
@@ -120,6 +120,7 @@ void ABullet::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrim
 		SurfaceRotator,
 		FVector(0.1f, 0.1f, 0.1f)
 		);
+	UE_LOG(LogTemp, Warning, TEXT("Bullet Hit %s"), *OtherComponent->GetName());
 	// 총알 제거
 	Destroy();
 }
